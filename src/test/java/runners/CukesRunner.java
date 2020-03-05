24dfed3c944e4a2ca34b26e6232bc3bf43cf00f6
wermax23 <56362428+wermax23@users.noreply.github.com>
2020-03-04 22:13:45 -0600

    Max final version Wednesday 03/04/19

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -1,4 +1,21 @@
 package runners;
 
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports",
+        features = "src/test/resources/features/student.feature",
+        glue= "step_definitions",
+        dryRun = false,
+        tags = "@TECP-251"
+)
+
+
+
 public class CukesRunner {
+
 }
